77724c7eba22522d64cfe6b0dd83fb16e75c74b2
Ogbunude Lemuel <lemuelcco@gmail.com>
2018-05-31 21:54:43 +0100

    add filter criteria to UI

--- app/src/main/java/com/example/lemuel/realmexample/MainActivity.java
+++ app/src/main/java/com/example/lemuel/realmexample/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.lemuel.realmexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,6 +13,7 @@ import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
+    //used ButterKnife library to reduce boilerplate code
     @BindView(R.id.txt_name)
     EditText name_txt;
     @BindView(R.id.txt_age)
@@ -22,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     Button add_btn;
     @BindView(R.id.txt_filter)
     TextView filter_txt;
+    @BindView(R.id.txt_ageFilter_number)
+    EditText age_filter_number_txt;
     @BindView(R.id.btn_filter)
     Button filter_btn;
 
@@ -41,25 +43,32 @@ public class MainActivity extends AppCompatActivity {
             // write operations in Realm must be wrapped in transactions.
             realm.beginTransaction();
             User user = realm.createObject(User.class); //todo  Create a new object
-            user.setName(name_txt.getText().toString()); //todo get user name
-            user.setAge(Integer.valueOf(age_txt.getText().toString())); //todo get user age
+            user.setName(name_txt.getText().toString()); //todo get user name from Edittext  and store in user object
+            user.setAge(Integer.valueOf(age_txt.getText().toString())); //todo get user age from Edittext  and store in user object
             realm.commitTransaction();
+            // commit transaction
 
-            Toast.makeText(this, "User added", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "User added", Toast.LENGTH_LONG).show();
             clearText();
         });
 
+
+        //query when filter button is clicked
         filter_btn.setOnClickListener(v -> {
                     //todo perform realm query
                     RealmResults<User> result = realm.where(User.class)
-                            .greaterThan("age", 5)
+                            //find all user whose age is greater than filter specified
+                            .lessThan("age", Integer.parseInt(age_filter_number_txt.getText().toString()))
+                            //return all result that reach criteria
                             .findAll();
 
+                    //loop through realm result and append the string builder
                     StringBuilder stringBuilder = new StringBuilder();
                     for (int i = 0; i < result.size(); i++) {
                         stringBuilder.append(result.get(i).getName() + "  ");
                     }
 
+                    //set text of the filtered-text Textview to that of the realm results
                     filter_txt.setText(stringBuilder.toString());
                 }
 
@@ -68,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //clear Textviews
     private void clearText() {
         name_txt.setText("");
         age_txt.setText("");
